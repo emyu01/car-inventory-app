ec7a0dbc832597edd90126619e213030159d2f9e
Umar <madugu01@gmail.com>
2021-01-19 09:41:42 +0100

    [ADD] added the fillable fields to allow bulk update of details

--- app/Models/Cars.php
+++ app/Models/Cars.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Cars extends Model
 {
     use HasFactory;
+
+    protected $table = "cars";
+    public $timestamps = true;
+
+    protected $fillable = [
+        'make', 'model', 'price', 'year', 'condition', 'remarks'
+    ];
 }
